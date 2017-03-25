2378e30a6c127aa80565a83fe0d11c88c160e2ca
Harloch <Dustin.Allen@harloch.com>
2017-03-25 12:31:48 -0500

    Initial Commit

---
+++ Event App/AddEventViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddEventViewController.swift
+//  Event App
+//
+//  Created by Dustin Allen on 3/25/17.
+//  Copyright © 2017 Harloch. All rights reserved.
+//
+
+import UIKit
+
+class AddEventViewController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var dateField: UITextField!
+    @IBOutlet var notificationSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func categoryButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func customizeBackground(_ sender: Any) {
+        
+    }
+    
+    @IBAction func notificationSwitchAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func dateAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addEvent(_ sender: Any) {
+        
+    }
+    
+}
