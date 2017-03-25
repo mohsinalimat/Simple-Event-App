ec511dfc42052f07bc092ed54af06bc7b667d4e3
Harloch <Dustin.Allen@harloch.com>
2017-03-25 14:31:26 -0500

    Calendar Added

---
+++ Event App/CalendarViewController.swift
@@ -0,0 +1,98 @@
+//
+//  CalendarViewController.swift
+//  Event App
+//
+//  Created by Dustin Allen on 3/25/17.
+//  Copyright © 2017 Harloch. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarViewController: UIViewController {
+    
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView") // Registering your cell is manditory
+        calendarView.cellInset = CGPoint(x: 0, y: 0)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension CalendarViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+            calendar: Calendar.current,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .sunday)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        // Setup text color
+        handleCellTextColor(view: cell, cellState: cellState)
+        handleCellSelection(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        handleCellSelection(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        handleCellSelection(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func handleCellTextColor(view: JTAppleDayCellView?, cellState: CellState) {
+        
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        
+        if cellState.isSelected {
+            myCustomCell.dayLabel.textColor = UIColor.gray
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                myCustomCell.dayLabel.textColor = UIColor.black
+            } else {
+                myCustomCell.dayLabel.textColor = UIColor.gray
+            }
+        }
+    }
+    
+    // Function to handle the calendar selection
+    func handleCellSelection(view: JTAppleDayCellView?, cellState: CellState) {
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        if cellState.isSelected {
+            myCustomCell.selectedView.layer.cornerRadius =  25
+            myCustomCell.selectedView.isHidden = false
+        } else {
+            myCustomCell.selectedView.isHidden = true
+        }
+    }
+}
---
+++ Event App/CellView.swift
@@ -0,0 +1,14 @@
+//
+//  CellView.swift
+//  Event App
+//
+//  Created by Dustin Allen on 3/25/17.
+//  Copyright © 2017 Harloch. All rights reserved.
+//
+
+import JTAppleCalendar
+class CellView: JTAppleDayCellView {
+    
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var selectedView: UIView!
+}
